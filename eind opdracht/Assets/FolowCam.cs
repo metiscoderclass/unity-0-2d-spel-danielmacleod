39d6ae8b7f129c1aa622bf3b9e1e1b7bede0116f
danielmacleod <31760492+danielmacleod@users.noreply.github.com>
2018-02-12 11:51:58 +0100

    Add files via upload

---
+++ eind opdracht/Assets/FolowCam.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FolowCam : MonoBehaviour {
+
+	public GameObject followTarget;
+	private Vector2 targetPos;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPos = new Vector2(followTarget.transform.position.x, followTarget.transform.position.y);
+			transform.position = Vector2.Lerp (transform.position, targetPos, moveSpeed);
+	}
+}
---
+++ eind opdracht/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+	
+public class PlayerMovement : MonoBehaviour {
+	
+	public Rigidbody2D rb;
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		if(Input.GetKey("d")){
+			rb.AddForce(new Vector2(20,2));
+		}
+		if(Input.GetKey("a")){
+			rb.AddForce(new Vector2(-20,2));
+		}
+		if(Input.GetKeyDown("w")){
+			rb.AddForce(new Vector2(0,500));
+		}
+		if(Input.GetKeyDown("s")){
+			rb.AddForce(new Vector2(0,-500));
+		}
+	}
+}
